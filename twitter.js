b4ebe7d44e9ec2287dfc74ce4399ab71e775869d
Amandine Perichon <apericho@gmail.com>
2016-07-29 00:06:29 +1200

    Twitter passport working

---
+++ twitter.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'apikey' : 'x9Xz5tr7d0GoDYT3EtU5Molb9',
+    'apisecret' : 'qkh6kIECd64QeUFfIBnKcuKq90b37V3BLGP16Ls1S91GqpgSCc',
+    'callbackUrl' : 'http://localhost:3000/login/twitter/callback'
+}
