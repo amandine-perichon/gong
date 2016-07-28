4c34555188f28343153457046c2b936e7e950c74
shsaci <shsaci@yahoo.com.au>
2016-07-28 21:32:31 +1200

    added skeleton cdn link and connected audio to start button

--- public/client.js
+++ public/client.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
-    $("button").click(function(event){
-        $("button").addClass('started')
-    })
+  $("button").click(function(event){
+    $("button").addClass('started')
+  })
+  var audioElement = document.createElement('audio');
+  audioElement.setAttribute('src', 'airhornStart.mp3');
+  audioElement.setAttribute('preload', 'auto');
+  //audioElement.load()
+
+  $.get();
+
+  audioElement.addEventListener("load", function() {
+    audioElement.play();
+  }, true);
+
+  $('.play').click(function() {
+    audioElement.play();
+  });
+
 })
